64245d2f953c1ecf380b9e9e3668f40a13425a03
PeterSonne <papa@deep2chill.de>
2020-02-17 13:59:18 +0800

    #030304 Added Animals class

--- classes.js
+++ classes.js
@@ -33,3 +33,25 @@ class Car {
 
 let car = new Car("Ford", "Focus", "black");
 console.log(car);
+
+// #30304
+
+class Animal {
+  constructor(name, verb, sound) {
+    this.name = name;
+    this.verb = verb;
+    this.sound = sound;
+  }
+  speak() {
+    console.log(
+      this.name +
+        " " +
+        (this.verb ? this.verb + "s" : " says '" + this.sound + "'")
+    );
+  }
+}
+new Animal("Dog", "bark", null).speak();
+new Animal("Cat", "meow").speak();
+new Animal("Cow", null, "moo").speak();
+new Animal("Lion", "roar", null).speak();
+new Animal("Duck", null, "quack").speak();
