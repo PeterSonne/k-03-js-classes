4834d887fc903c47d7beec0a541050d0627e5f3a
PeterSonne <papa@deep2chill.de>
2020-02-17 13:35:41 +0800

    #030302 Added Student class

--- classes.js
+++ classes.js
@@ -7,3 +7,16 @@ class Bike {
 }
 
 console.log(new Bike());
+
+// #030302
+
+class Student {
+  constructor(firstName, lastName, age) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+  }
+}
+
+let student = new Student("John", "Smith", 18);
+console.log(student);
