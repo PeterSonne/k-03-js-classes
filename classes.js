2c8dfb0eab7c48083b458cc9568da27eaf6135e2
PeterSonne <papa@deep2chill.de>
2020-02-17 14:56:22 +0800

    #30308 Added Point class

--- classes.js
+++ classes.js
@@ -163,3 +163,17 @@ const getReceipt = (labels, prices, discounts) => {
   console.log("-".padEnd(13 + wordLen, "-"));
 };
 getReceipt(labels, prices, discounts);
+
+// #30308
+
+class Point {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+  plus(point) {
+    return new Point(this.x + point.x, this.y + point.y);
+  }
+}
+
+console.log(new Point(1, 2).plus(new Point(2, 1)));
